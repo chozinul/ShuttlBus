c768b8c95457f5f238331d33ff68bfb6b7bce187
Amri <chozinul@gmail.com>
2018-05-17 16:24:34 +0800

    added bus stop activity

---
+++ ShuttleBus/View/BusStopCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BusStopCollectionViewCell.swift
+//  ShuttleBus
+//
+//  Created by Muchamad Chozinul Amri on 17/5/18.
+//  Copyright © 2018 Muchamad Chozinul Amri. All rights reserved.
+//
+
+import UIKit
+
+class BusStopCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var rightArrow: UIImageView!
+    
+}
--- ShuttleBus/ViewController/BusStopViewController.swift
+++ ShuttleBus/ViewController/BusStopViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class BusStopViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    fileprivate var busStops = [BusStop]()
+    fileprivate let reuseIdentifier = "BusStopCollectionViewCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+    fileprivate let itemsPerRow: CGFloat = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         ShuttleBusAPI.GetBusStop(completion: {
-            (busStopListAPI) in
-            print("finish")
+            [unowned self] (busStopListAPI)  in
+            if let busStops = busStopListAPI?.busStopsResult?.busStops {
+                self.busStops = busStops
+                self.collectionView.reloadData()
+            }
         })
     }
 
@@ -24,6 +34,68 @@ class BusStopViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+// MARK: - UICollectionViewDelegate
+extension BusStopViewController : UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath){
+        
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension BusStopViewController: UICollectionViewDataSource {
+    //1
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //2
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return busStops.count
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath) as! BusStopCollectionViewCell
+        
+        let busStop = busStops[indexPath.row]
+        cell.thumbnail.image = UIImage(named: "bus_stop")
+        cell.rightArrow.image = UIImage(named: "right_arrow")
+        cell.title.text = busStop.caption
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension BusStopViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: 70)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+}
